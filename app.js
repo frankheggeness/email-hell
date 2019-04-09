22328194c7b77f55e05314272462366f4fbc44d8
Frank <fr.heggeness@gmail.com>
2019-04-09 09:38:54 -1000

    done

---
+++ app.js
@@ -0,0 +1,20 @@
+
+
+function emailFinder() {
+  let dataset = require('./email-log.json')
+  const findEmailAddress = dataset.emails.reduce(function (prev, current, index, array) {
+    if (prev.hasOwnProperty(current.email)) {
+      prev[current.email]++
+    } else {
+      prev[current.email] = 1
+    }
+    return prev
+  }, {})
+  return {
+    findEmailAddress,
+  }
+}
+
+let emailModule = emailFinder();
+
+console.log(emailModule.findEmailAddress)
